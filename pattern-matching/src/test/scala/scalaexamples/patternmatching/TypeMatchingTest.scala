963186bacdb2a8335bd3785ffbce87303c1475da
Trond Marius Ovstetun <trond@ovstetun.no>
2010-01-27 14:37:05 +0100

    TypeMatchingTest

--- pattern-matching/src/test/scala/scalaexamples/patternmatching/TypeMatchingTest.scala
+++ pattern-matching/src/test/scala/scalaexamples/patternmatching/TypeMatchingTest.scala
@@ -8,17 +8,21 @@ import scalaexamples.EmptyTest
 
 @RunWith(classOf[JUnit4])
 class TypeMatchingTest extends EmptyTest {
-  
-  // @Test 
+
+  @Test
   def matchOnType {
-	val elements = List(23, "Hello", 8.5, 'q') 
-	for (element <- elements) { 
-	  element match { 
-	    // Make test run, and assert on types, e.g. " => assertEquals(23, integer)" or " => assertEquals("Hello", s)"
-	    // Insert your matching code here
-	    case other => error("Should match other on type")
-	 }
-	}
+    val elements = List(23, "Hello", 8.5, 'q')
+    for (element <- elements) {
+      element match {
+        // Make test run, and assert on types, e.g. " => assertEquals(23, integer)" or " => assertEquals("Hello", s)"
+        // Insert your matching code here
+        case integer:Int => assertEquals(23, integer)
+        case d:Double => assertEquals(8.5, d)
+        case s:String => assertEquals("Hello", s)
+        case c:Character => assertEquals('q', c)
+        case other => error("Should match other on type" + other)
+      }
+    }
   }
 
 }
