7392c1de856a3a1733108de62ae248f07d973540
Trond Marius Ovstetun <trond@ovstetun.no>
2010-02-03 09:32:28 +0100

    ShapeTest complete

--- oo-traits/src/main/scala/scalaexamples/inheritance/Circle.scala
+++ oo-traits/src/main/scala/scalaexamples/inheritance/Circle.scala
@@ -3,13 +3,15 @@ package scalaexamples.inheritance
 import Math.Pi
 
 // Make Circle extend Shape, and add a primary constructor for radius and color
-class Circle {
+class Circle(val radius: Double, color: String) extends Shape(color) {
 
   // Create a auxiliary constructor
+  def this(radius:Double) = this(radius, Shape.DefaultColor)
   
   // Define the area function
+  def area = Pi * radius * radius
   
   // Make this compile
-  // def circumference = 2 * Pi * radius
+  def circumference = 2 * Pi * radius
   
 }
